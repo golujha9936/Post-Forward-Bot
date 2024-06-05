fb298fd91d0a719d9e08cdfa30f3903b902d1c4b
Evo^xᴅ 𓆩🇮🇳𓆪 <121981620+golujha9936@users.noreply.github.com>
2024-06-06 05:26:10 +0530

    UFF!!

--- translation.py
+++ translation.py
@@ -1,13 +1,13 @@
 class Translation(object):
 
       # Start text
-      START = "Hi there! I am a channel auto forward bot. I can forward new messages & Media from one channel to another.\n\n• Source code:<b/> <b><a href='https://t.me/About_Aryan_Owner'>Auto Forward Bot</a><b/>"             
+      START = "ʜɪ ᴛʜᴇʀᴇ! ɪ ᴀᴍ ᴀ ᴄʜᴀɴɴᴇʟ ᴀᴜᴛᴏ ғᴏʀᴡᴀʀᴅ ʙᴏᴛ. ɪ ᴄᴀɴ ғᴏʀᴡᴀʀᴅ ɴᴇᴡ ᴍᴇssᴀɢᴇs & ᴍᴇᴅɪᴀ ғʀᴏᴍ ᴏɴᴇ ᴄʜᴀɴɴᴇʟ ᴛᴏ ᴀɴᴏᴛʜᴇʀ.\n\n• Aʙᴏᴜᴛ Mᴇ::<b/> <b><a href='https://t.me/About_Aryan_Owner'>ᗰЄ🥂💘</a><b/>"             
 
       #About text
       ABOUT = """
-<b>📝 Language:</b> Python 3
-<b>🧰 Framework:</b> Pyrogram
-<b>👨‍💻 Developer:</b> ─‌⃛┼𝆺𝅥⃝ᶦϻ‌ ᵛ‌ᵎᵖ ≛⃝ 𝐀𝐫𝐲𝐚𝐧❣️𓆩𝐎𝐰𝐧𝐞𝐫𓆪ꪾ🥀⃝⃪•๋๋๋๋๋๋๋๋๋๋๋๋๋•๋
-<b>📢 Channel:</b> @Ace_networkop
-<b>👥 Group:</b> @About_Aryan_Owner
-<b>🌐Source Code:</b> Press Me 🥰"""
+<b>📝 𝐋ᴀɴɢᴜᴀɢᴇ: 𝐏ʏᴛʜᴏɴ 𝟹
+<b>🧰 𝐅ʀᴀᴍᴇ𝐖ᴏʀᴋ: 𝐏ʏʀᴏɢʀᴀᴍ
+<b>👨‍💻 𝐃ᴇᴠᴇʟᴏᴘᴇʀ: <a href='https://t.me/FAKE_ARYAN'>─‌⃛┼𝆺𝅥⃝ᶦϻ‌ ᵛ‌ᵎᵖ ≛⃝ 𝐀𝐫𝐲𝐚𝐧❣️𓆩𝐎𝐰𝐧𝐞𝐫𓆪ꪾ🥀⃝⃪•๋๋๋๋๋๋๋๋๋๋๋๋๋•๋ 🇮🇳</a> 
+<b>📢 𝐂ʜᴀɴɴᴇʟ: <a href='https://t.me/Ace_networkop'>𝐀ᴄᴇ 𝐍ᴇᴛᴡᴏʀᴋ [🇮🇳]</a> 
+<b>👥 𝐆ʀᴏᴜᴘ: <a href='https://t.me/About_Aryan_Owner'>ᗰЄ🥂💘</a> 
+<b>🌐 𝐁ᴜɪʟᴅ 𝐒ᴛᴀᴛᴜs : ᴠ2.7.1 [sᴛᴀʙʟᴇ]"""
